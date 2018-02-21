ffee881a75d67c1401299df16e51c6f11970e6ec
thirubala3171@ssh.imad.hasura-app.io <thirubala31@gmail.com>
2018-02-21 23:59:56 +0530

    Implementing the changes for python app

--- microservices/api/src/__init__.py
+++ microservices/api/src/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-
+CORS(app)
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
 from .hasura import hasura_examples
--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -1,13 +1,79 @@
 from src import app
-# from flask import jsonify
-
+from flask import jsonify, json
+from flask import Flask,render_template,make_response
+from flask import request
+import requests
 
 @app.route("/")
 def home():
     return "Hasura Hello World"
 
+@app.route("/hello")
+def hello_world():
+	return "Hello World - Thiru"
 # Uncomment to add a new URL at /new
 
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
+@app.route("/form",methods=['POST'])
+def form():
+	if request.method == 'POST': #this block is only entered when the form is submitted
+		metric = request.form.get('metric')
+		return '''<h1>The metric value is: {}</h1>'''.format(metric)
+
+	return '''<form method="POST">
+            Metric: <input type="text" name="metric"><br>
+            <input type="submit" value="Submit"><br>
+            </form>'''
+	
+@app.route("/getmetric",methods=['POST'])
+def get_metric():
+	#get the metric from front-end
+	jsonMetric = request.get_json()
+	
+	#converting to json object
+	jsonData = json.loads(jsonMetric)
+	
+	metric = jsonData['metric']
+
+	# This is the url to which the query is made
+	url = "https://data.declassification29.hasura-app.io/v1/query"
+
+	# This is the json payload for the query
+	requestPayload = {
+		"type": "select",
+		"args": {
+			"table": "world_happiness_report",
+			"columns": [
+				"*"
+			],
+			"order_by": [
+				{
+					"column": "ID"
+				}
+			]
+		}
+	}
+
+	# Setting headers
+	headers = {
+		"Content-Type": "application/json"
+	}
+
+	# Make the query and store response in resp
+	dbresp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+	
+	#converting the response to json object
+	respjson = json.loads(dbresp.content)
+	
+	result = {}	
+	i = 0
+	#looping through the list object
+	for index, value in enumerate(respjson):
+		#looping through the dict  using dict comprehension
+		temp = {x:y for x,y in value.items() if x in ('Country',metric)}
+		result[i] = {}
+		result[i] = temp
+		i += 1
+	
+	anslist = json.dumps(result)
+	
+	return anslist
